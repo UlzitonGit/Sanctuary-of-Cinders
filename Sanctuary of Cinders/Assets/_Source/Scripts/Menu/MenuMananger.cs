9b60cd3f1df81b58e4988223cd0eac0d678345b1
Uzel <156241117+UlzitonGit@users.noreply.github.com>
2025-06-04 20:15:48 +0300

    npc

--- Sanctuary of Cinders/Assets/_Source/Scripts/Menu/MenuMananger.cs
+++ Sanctuary of Cinders/Assets/_Source/Scripts/Menu/MenuMananger.cs
@@ -3,9 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class MenuMananger : MonoBehaviour
 {
-   
+    private string _pathDest = "data.txt";
     public void Play()
     {
         SceneManager.LoadScene(2);
     }
+
+    public void PlayNewGame()
+    {
+        SaveSystem.Delete();
+        SceneManager.LoadScene(2);
+    } 
 }
